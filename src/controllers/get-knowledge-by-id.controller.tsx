1a9b08a323845d4bf52126a076aa143512c0f585
masato <furuyamasato6@gmail.com>
2025-09-14 17:17:03 +0900

    add: 詳細表示機能の追加(MarkDown形式に表示することが出来ません)

---
+++ src/controllers/get-knowledge-by-id.controller.tsx
@@ -0,0 +1,10 @@
+import { KnowledgeDetailFeature } from '../features/KnowledgeDetailFeature.js';
+import { KnowledgeRepository } from '../models/knowledge.repository.js';
+
+export async function getKnowledgeByIdController(userId: string, knowledgeId: string) {
+  console.log('Signed-in as', userId);
+
+  const knowledge = await KnowledgeRepository.getByKnowledgeId(knowledgeId);
+
+  return <KnowledgeDetailFeature knowledge={knowledge} />;
+}
